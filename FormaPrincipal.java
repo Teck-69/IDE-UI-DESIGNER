9eed43855de4f7d2a801390a045dbf6bf128c281
fabio-ck32 <71847891+fabio-ck32@users.noreply.github.com>
2020-10-14 10:04:29 -0400

    Add files via upload

---
+++ FormaPrincipal.java
@@ -0,0 +1,116 @@
+package com.fabiosilva.clases;
+import android.graphics.Color;
+import android.widget.Toast;
+import android.content.Context;
+
+public class FormaPrincipal {
+    public int tag; //é um id
+    public int width;//largura
+    public int heigth;//altura
+    public float posx;//posiçao x
+    public float posy;//posição y
+    public FormaPrincipal parent;//pai
+
+class tag extends FormaPrincipal{
+    
+}
+class limear_h extends FormaPrincipal{
+   public int id;//id
+   public String width;//largura
+   public String height;//altura
+   public int fill_left;//preencimento
+   public int fill_right;//preenchimento
+   public int fill_top;//preenchimento
+   public int fill_button;//preenchimento
+   public int margin_left;// margem
+   public int margin_right;//margem
+   public int margin_top;//margem
+   public int margin_button;//margem
+   public String guidance;//orientação
+   public int Weight;//peso
+   public String background_resource;// imagem de fundo
+   public Color background_color;// cor de fundo
+   public int to_spin;//rotação
+   public int alpha;//transparência
+   public int transition_x;//tranzição x
+   public int transition_y;//tranzição y
+   public int scale_x;//escala x
+   public int scale_y;//escala y
+}
+
+class linear_v extends FormaPrincipal{
+    
+}
+
+ class scroll_h extends FormaPrincipal{
+    
+}
+    
+ class scroll_v extends FormaPrincipal{
+    
+}
+    
+class textview extends FormaPrincipal{
+    
+}
+
+class edittext extends FormaPrincipal{
+    
+}
+
+class button extends FormaPrincipal{
+    
+}
+
+class imageview extends FormaPrincipal{
+}
+
+class videoview extends FormaPrincipal{
+    
+}
+
+class checkbox extends FormaPrincipal{
+    
+}
+
+class spiner extends FormaPrincipal{
+    
+}
+
+class listview extends FormaPrincipal{
+    
+}
+
+class webview extends FormaPrincipal{
+    
+}
+
+class swiitch extends FormaPrincipal{
+    
+}
+
+class seekbar extends FormaPrincipal{
+    
+}
+
+class progressbar extends FormaPrincipal{
+    
+}
+
+class calendarview extends FormaPrincipal{
+    
+}
+
+class adview extends FormaPrincipal{
+    
+}
+
+class mapview extends FormaPrincipal{
+    
+}
+
+class adView extends FormaPrincipal{
+    
+}
+}
+
